6771aeff32bea82419802678ebc071b65fcc96ce
Charline Ramelet <charline.ramelet@gmail.com>
2022-07-26 12:58:02 +0200

    Réorganisation recipe factory

--- factories/recipe-factory.js
+++ factories/recipe-factory.js
@@ -1,15 +1,18 @@
+// Objet modèle d'une card recette
 const recipeFactory = {
-
+  /**
+   * Fonction qui génère les data dans la card recette
+   * 
+   * @param {*} data 
+   * @param {*} i 
+   */
     displayRecipe(data, i) {
 
-
-
-      const col = document.createElement("div");
-      
+      const col = document.createElement("div");      
       col.classList.add("border-white", "col");
-
       const recipesContainer = document.querySelector(".recipes");      
 
+      // Ajout du contenu html
       col.innerHTML = `
         <div class="card">
           <div class="img-container">
@@ -19,7 +22,6 @@ const recipeFactory = {
               alt="..."
             />
           </div>
-
           <div class="card-body height-200">
             <div class="d-flex justify-content-between">
               <h5 class="card-title">${data.name}</h5>
@@ -30,8 +32,7 @@ const recipeFactory = {
             </div>
             <div class="d-flex w-100">
               <div class="col col-text pe-2">
-                <ul class="d-flex flex-column p-0 ingredient-list-${i}">
-                  
+                <ul class="d-flex flex-column p-0 ingredient-list-${i}">                  
                 </ul>
               </div>
               <div class="col col-text card-description p-0 w-100">
@@ -42,23 +43,17 @@ const recipeFactory = {
         </div>
       `;
       
-      recipesContainer.appendChild(col);
-      
+      recipesContainer.appendChild(col);      
 
-      const ingredients = data.ingredients;
-      
+      const ingredients = data.ingredients;      
       const ingredientContainer = document.querySelector(".ingredient-list-"+i);
 
       for(let i=0; i < ingredients.length; i++) {
 
         const ingredientElem = document.createElement("li");
-        ingredientElem.classList.add("d-flex", "flex-row", "align-items-end", "ingredient-"+i);
-
-        ingredientContainer.appendChild(ingredientElem);
-
-        let liChild = document.createElement("div");
+        const liChild = document.createElement("div");
+        ingredientElem.classList.add("d-flex", "flex-row", "align-items-end", "ingredient-"+i);        
         liChild.classList.add("d-flex", "m-0", "ingredient", "elem-"+i);
-
         let quantity = "";
         let unit = "";
 
@@ -74,8 +69,8 @@ const recipeFactory = {
           <span class="fw-bold">${ingredients[i].ingredient}&thinsp;</span>
           ${quantity} ${unit}
         `
+        ingredientContainer.appendChild(ingredientElem);
         ingredientElem.appendChild(liChild);
-
       }
     }
 }
