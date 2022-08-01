f52ed597aca33d98e1f2b6e247c93e6e5594f492
Charline Ramelet <charline.ramelet@gmail.com>
2022-08-01 16:55:04 +0200

    Modifie gestion des tags

--- factories/recipe-factory.js
+++ factories/recipe-factory.js
@@ -2,18 +2,17 @@
 const recipeFactory = {
   /**
    * Fonction qui génère les data dans la card recette
-   * 
-   * @param {*} data 
-   * @param {*} i 
+   *
+   * @param {*} data
+   * @param {*} i
    */
-    displayRecipe(data, i) {
+  displayRecipe(data, i) {
+    const col = document.createElement("div");
+    col.classList.add("border-white", "col");
+    const recipesContainer = document.querySelector(".recipes");
 
-      const col = document.createElement("div");      
-      col.classList.add("border-white", "col");
-      const recipesContainer = document.querySelector(".recipes");      
-
-      // Ajout du contenu html
-      col.innerHTML = `
+    // Ajout du contenu html
+    col.innerHTML = `
         <div class="card">
           <div class="img-container">
             <img
@@ -42,35 +41,39 @@ const recipeFactory = {
           </div>
         </div>
       `;
-      
-      recipesContainer.appendChild(col);      
 
-      const ingredients = data.ingredients;      
-      const ingredientContainer = document.querySelector(".ingredient-list-"+i);
+    recipesContainer.appendChild(col);
 
-      for(let i=0; i < ingredients.length; i++) {
+    const ingredients = data.ingredients;
+    const ingredientContainer = document.querySelector(".ingredient-list-" + i);
 
-        const ingredientElem = document.createElement("li");
-        const liChild = document.createElement("div");
-        ingredientElem.classList.add("d-flex", "flex-row", "align-items-end", "ingredient-"+i);        
-        liChild.classList.add("d-flex", "m-0", "ingredient", "elem-"+i);
-        let quantity = "";
-        let unit = "";
+    for (let i = 0; i < ingredients.length; i++) {
+      const ingredientElem = document.createElement("li");
+      const liChild = document.createElement("div");
+      ingredientElem.classList.add(
+        "d-flex",
+        "flex-row",
+        "align-items-end",
+        "ingredient-" + i
+      );
+      liChild.classList.add("d-flex", "m-0", "ingredient", "elem-" + i);
+      let quantity = "";
+      let unit = "";
 
-        if(ingredients[i].quantity) {
-          quantity = ":&thinsp;"+ingredients[i].quantity;
-        }
+      if (ingredients[i].quantity) {
+        quantity = ":&thinsp;" + ingredients[i].quantity;
+      }
 
-        if(ingredients[i].unit) {
-          unit = ingredients[i].unit;
-        }
-        
-        liChild.innerHTML = `
+      if (ingredients[i].unit) {
+        unit = ingredients[i].unit;
+      }
+
+      liChild.innerHTML = `
           <span class="fw-bold">${ingredients[i].ingredient}&thinsp;</span>
           ${quantity} ${unit}
-        `
-        ingredientContainer.appendChild(ingredientElem);
-        ingredientElem.appendChild(liChild);
-      }
+        `;
+      ingredientContainer.appendChild(ingredientElem);
+      ingredientElem.appendChild(liChild);
     }
-}
+  },
+};
