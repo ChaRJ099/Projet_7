7b0861b075cc29c635b10e82847bc744ee95cc74
Charline Ramelet <charline.ramelet@gmail.com>
2022-08-09 17:23:03 +0200

    Ajout des commentaires

--- filter.js
+++ filter.js
@@ -2,23 +2,24 @@ function filterRecipe(searchValue) {
   // init calcul temps
   let t0 = performance.now();
 
+  // Méthode filter sur l'objet de données searchResults. Retourne un tableau filtré (chaque recette avec isValue = true est ajouté à searchResults)
   searchResults = searchResults.filter((result) => {
     let isValue = false;
-
+    // Regarde s'il y a correspondance entre searchValue et result.name. Si oui, retourne "isValue = true". Sinon, retourne isValue = false.
     if (result.name.indexOf(searchValue) != -1) {
       return (isValue = true);
     }
-
+    // Regarde s'il y a correspondance entre searchValue et result.description. Si oui, retourne "isValue = true". Sinon, retourne isValue = false.
     if (result.description.indexOf(searchValue) != -1) {
       return (isValue = true);
     }
-
+    // Regarde s'il y a correspondance entre searchValue et result.ingredient. Si oui, retourne "isValue = true". Sinon, retourne isValue = false.
     result.ingredients.forEach((ingredient) => {
       if (ingredient.ingredient.indexOf(searchValue) != -1) {
         return (isValue = true);
       }
     });
-
+    // Retourne isValue
     return isValue;
   });
 
